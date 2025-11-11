7bf1cc5f69060460bcce08820ac5e3fbcacbbe05
Suraksha <surakshak571@gmail.com>
2025-11-11 20:03:10 +0500

    ok

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const io = socketIo(server, {
 app.use(cors({
   origin: process.env.CLIENT_ORIGIN ? process.env.CLIENT_ORIGIN.split(',') : '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
 }));
